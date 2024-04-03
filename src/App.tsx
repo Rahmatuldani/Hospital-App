d3b0d185b6a6376e7c4ef832ac081700efcd0b07
Rahmatuldani <danirahmatul@gmail.com>
2024-04-03 15:23:44 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <div>App Page</div>
+  );
+}
+
+export default App;
---
+++ src/routers/index.tsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
+import App from "../App";
+import Login from "../pages/auth/login";
+
+const Routers = createBrowserRouter(
+    createRoutesFromElements(
+        <>
+            <Route path="/" element={<App/>}></Route>
+            <Route path="/login" element={<Login/>}></Route>
+        </>
+    )
+)
+
+export default Routers;
